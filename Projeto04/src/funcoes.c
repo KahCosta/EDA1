38684699a34b8a248d9017da1f9f8de31d899905
KahCosta <kamillacosta.unb@gmail.com>
2018-06-04 14:54:19 -0300

    Ordenando por meio do combustivel

--- Projeto04/src/funcoes.c
+++ Projeto04/src/funcoes.c
@@ -32,6 +32,9 @@ int sorteiaNumero(int *combA, int *vetInformacoes){
   for(int i = 0; i < nAproximacoes; i++){
     numSorteadoCombA = ((rand() % NUMMAXACOMBUSTIVEL) + 1);
     *(combA+i) = numSorteadoCombA;
+  }
+  ordenaPrioridade(combA, nAproximacoes);
+  for(int i = 0; i < nAproximacoes; i++){
     printf("Aviao %d = %d \n",i+1, combA[i]);
   }
   *(vetInformacoes + 0) = nVoos;
@@ -53,6 +56,20 @@ void selecionaCodigosVoos(int *vetInformacoes, char *codVoos[], char *codAproxim
     codDecolagens[i] = codVoos[i+(nAproximacoes)];
     printf("\t %s", codDecolagens[i]);
   }
+}
+
+void ordenaPrioridade( int *vetor, int TAM){
+  int x,y;
+  int aux;
+  for( x = 0; x < TAM; x++ ){
+    for( y = x + 1; y < TAM; y++ ){
+      if ( vetor[x] > vetor[y] ){
+           aux = vetor[x];
+           vetor[x] = vetor[y];
+           vetor[y] = aux;
+        }
+      }
+    }
 }
 
 Fila* criaFilaAproximacao(){
